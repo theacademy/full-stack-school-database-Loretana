b32a298be6015c854640cecef3c0af1c7aa897a4
Loretana <lmgsbusiness@gmail.com>
2025-03-19 13:39:41 -0400

    Implement TeacherDao & Mapper & change Java version to 11

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/TeacherDaoImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/TeacherDaoImpl.java
@@ -2,9 +2,11 @@ package mthree.com.fullstackschool.dao;
 
 import mthree.com.fullstackschool.dao.mappers.TeacherMapper;
 import mthree.com.fullstackschool.model.Teacher;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
@@ -19,11 +21,22 @@ public class TeacherDaoImpl implements TeacherDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
     @Override
+    @Transactional
     public Teacher createNewTeacher(Teacher teacher) {
         //YOUR CODE STARTS HERE
+        final String INSERT_TEACHER = "INSERT INTO teacher(tFName, tLName, dept) "
+                + "VALUES(?, ?, ?)";
+        jdbcTemplate.update(INSERT_TEACHER,
+                teacher.getTeacherFName(),
+                teacher.getTeacherLName(),
+                teacher.getDept());
 
-        return null;
+        final String GET_LATEST_ID = "SELECT tid FROM teacher ORDER BY tid DESC LIMIT 1";
+        int newId = jdbcTemplate.queryForObject(GET_LATEST_ID, Integer.class);
+        teacher.setTeacherId(newId);
+        return teacher;
 
         //YOUR CODE ENDS HERE
     }
@@ -31,8 +44,8 @@ public class TeacherDaoImpl implements TeacherDao {
     @Override
     public List<Teacher> getAllTeachers() {
         //YOUR CODE STARTS HERE
-
-        return null;
+        final String SELECT_ALL_TEACHERS = "SELECT * FROM teacher";
+        return jdbcTemplate.query(SELECT_ALL_TEACHERS, new TeacherMapper());
 
         //YOUR CODE ENDS HERE
     }
@@ -40,8 +53,12 @@ public class TeacherDaoImpl implements TeacherDao {
     @Override
     public Teacher findTeacherById(int id) {
         //YOUR CODE STARTS HERE
-
-        return null;
+        try {
+            final String SELECT_TEACHER_BY_ID = "SELECT * FROM teacher WHERE tid = ?";
+            return jdbcTemplate.queryForObject(SELECT_TEACHER_BY_ID, new TeacherMapper(), id);
+        } catch (DataAccessException ex) {
+            return null;
+        }
 
         //YOUR CODE ENDS HERE
     }
@@ -49,15 +66,21 @@ public class TeacherDaoImpl implements TeacherDao {
     @Override
     public void updateTeacher(Teacher t) {
         //YOUR CODE STARTS HERE
-
-
+        final String UPDATE_TEACHER = "UPDATE teacher SET tFName = ?, tLName = ?, dept = ? "
+                + "WHERE tid = ?";
+        jdbcTemplate.update(UPDATE_TEACHER,
+                t.getTeacherFName(),
+                t.getTeacherLName(),
+                t.getDept(),
+                t.getTeacherId());
         //YOUR CODE ENDS HERE
     }
 
     @Override
     public void deleteTeacher(int id) {
         //YOUR CODE STARTS HERE
-
+        final String DELETE_TEACHER = "DELETE FROM teacher WHERE tid = ?";
+        jdbcTemplate.update(DELETE_TEACHER, id);
 
         //YOUR CODE ENDS HERE
     }
